18d784c6c4da4a8d5e31ecd2c6c0dbdc5d98a517
hn-88 <hn-88@users.noreply.github.com>
2026-09-01 09:36:44 +0530

    Create version.php - mandatory for moodle plugin
    on comparing with https://github.com/hn-88/moodle-plugin-local-referrals and asking Claude

---
+++ local/swayamplus/version.php
@@ -0,0 +1,5 @@
+  defined('MOODLE_INTERNAL') || die();
+  $plugin->component = 'local_swayamplus';
+  $plugin->version   = 2025090100;
+  $plugin->requires  = 2022041900; // minimum Moodle version
+  $plugin->maturity  = MATURITY_STABLE;
